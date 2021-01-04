7207c22666bc40cd830dde089d46dcd69550da99
Tsung-Yin Lin <lukelin@tsung-yins-mbp.home>
2021-01-03 20:10:19 -0500

    show the paragraph to read

--- taiwanese/config.py
+++ taiwanese/config.py
@@ -9,3 +9,4 @@ with open(config_path, 'r') as stream:
 
 output_folder = config["server"]["output_folder"]
 g_cred_path = config["credentials"]["google"]
+material_g_folder_id = config["drive_folder_ids"]["materials_folder"]
--- taiwanese/hw/homework.py
+++ taiwanese/hw/homework.py
@@ -1,9 +1,14 @@
 from taiwanese.config import *
 from taiwanese.back.student_list import HandleStudentList
 from flask import Blueprint, render_template, request
+from taiwanese.back.utils.drive import GDrive
+from taiwanese.back.utils.doc import GDoc
 
 hw_bp = Blueprint("hw", __name__)
 hsl = HandleStudentList(config_path)
+gdrive = GDrive(config_path)
+gdoc = GDoc(config_path)
+
 
 @hw_bp.route("/<string:week>/<string:student_id>/", methods=['GET', 'POST'])
 def submit(week, student_id):
@@ -14,6 +19,9 @@ def submit(week, student_id):
     name = found[1]
     email = found[2]
 
+    doc_id = [r for r in gdrive.list_files(material_g_folder_id) if r["name"] == '20200104'][0]["id"]
+    paragraph = gdoc.load_doc(doc_id)
+
     if request.method == "POST":
         f = request.files['audio_data']
         tmp_file = "{}/{}.wav".format(output_folder, student_id)
@@ -22,6 +30,10 @@ def submit(week, student_id):
             f.save(audio)
         print('file uploaded successfully')
 
-        return render_template('assignment.html', name=name, week=week, student_id=student_id, equest="POST")
+        return render_template(
+            'assignment.html', name=name, week=week, paragraph=paragraph,
+            student_id=student_id, equest="POST")
     else:
-        return render_template("assignment.html", name=name, week=week, student_id=student_id)
+        return render_template(
+            "assignment.html", name=name, week=week, paragraph=paragraph,
+            student_id=student_id)
