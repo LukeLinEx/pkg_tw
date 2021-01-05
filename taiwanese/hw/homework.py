25b8c1b0fde6ed26de9fbd711028da7184ff596c
Tsung-Yin Lin <lukelin@tsung-yins-mbp.home>
2021-01-04 20:38:15 -0500

    to s3

--- taiwanese/hw/homework.py
+++ taiwanese/hw/homework.py
@@ -4,11 +4,16 @@ from flask import Blueprint, render_template, request
 from taiwanese.back.utils.drive import GDrive
 from taiwanese.back.utils.doc import GDoc
 
+import boto3
+from botocore.exceptions import ClientError
+
 hw_bp = Blueprint("hw", __name__)
 hsl = HandleStudentList(config_path)
 gdrive = GDrive(config_path)
 gdoc = GDoc(config_path)
 
+aws_session = boto3.Session(profile_name='twta')
+s3_client = aws_session.client('s3')
 
 @hw_bp.route("/<string:week>/<string:student_id>/", methods=['GET', 'POST'])
 def submit(week, student_id):
@@ -19,7 +24,7 @@ def submit(week, student_id):
     name = found[0][1]
     email = found[0][2]
 
-    doc_id = [r for r in gdrive.list_files(material_g_folder_id) if r["name"] == '20200104'][0]["id"]
+    doc_id = [r for r in gdrive.list_files(material_g_folder_id) if r["name"] == week][0]["id"]
     paragraph = gdoc.load_doc(doc_id)
 
     if request.method == "POST":
@@ -29,6 +34,9 @@ def submit(week, student_id):
         with open(tmp_file, 'wb') as audio:
             f.save(audio)
         print('file uploaded successfully')
+
+        s3_client.upload_file(tmp_file, Bucket="taiwanese", Key="{}/{}".format(week, student_id))
+        os.remove(tmp_file)
 
         return render_template(
             'assignment.html', name=name, week=week, paragraph=paragraph,
