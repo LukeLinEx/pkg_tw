a6c572536e6d004e8cd1bb86e31a4d4141f41d3c
Tsung-Yin Lin <lukelin@tsung-yins-mbp.home>
2021-01-09 20:46:11 -0500

    order of the instructor's summary

--- taiwanese/instructor/review.py
+++ taiwanese/instructor/review.py
@@ -15,34 +15,11 @@ def summary():
     hsl = HandleStudentList()
     students = pd.DataFrame(hsl.axv[1:], columns=hsl.axv[0])
 
-    output = sub.merge(students, on="student_id").sort_values(["week", "name"])
+    output = sub.merge(students, on="student_id")
     output["ref"] = "/homework/submission/" + output[["student_id", "week", "name"]].agg('/'.join, axis=1)
 
     lst = list(
         output[['week', 'name', 'ref']].groupby("week"))
+    lst = sorted(lst, key=lambda rec: rec[0], reverse=True)
 
     return render_template("instructor_summary.html", lst=lst)
-
-
-def test():
-    hsb = HandleSubmission()
-    sub = hsb.get_submission_df()
-
-    hsl = HandleStudentList()
-    students = pd.DataFrame(hsl.axv[1:], columns=hsl.axv[0])
-
-    output = sub.merge(students, on="student_id").sort_values(["week", "name"])
-    output["ref"] = "/homework/submission/" + output[["student_id", "week", "name"]].agg('/'.join, axis=1)
-
-    lst = list(
-        output[['week', 'name', 'ref']].groupby("week")
-    )
-    return lst
-
-
-if __name__ == "__main__":
-    a = test()
-    for k, v in a:
-        print(k)
-        print(v.values)
-        print("\n")
